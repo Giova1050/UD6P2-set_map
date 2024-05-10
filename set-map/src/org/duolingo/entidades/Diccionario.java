a2921761b455062a854b6a48a2d5fc3285de715a
Giovanni <usr3855@salesianos-lacuesta.net>
2024-05-10 20:19:20 +0100

    creacion añadirPalabra

--- set-map/src/org/duolingo/entidades/Diccionario.java
+++ set-map/src/org/duolingo/entidades/Diccionario.java
@@ -1,5 +1,21 @@
 package org.duolingo.entidades;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.swing.JOptionPane;
+
 public class Diccionario {
-    
+    private Map<String, Set<String>> diccionario = new HashMap<>();
+
+    public void añadirPalabra() {
+        String nuevaPalabra = JOptionPane.showInputDialog(null, "Introduce una nueva palabra", "Nueva Palabra", 0);
+        if (!Validador.sanitanizacion(nuevaPalabra)) {
+            JOptionPane.showMessageDialog(null, "Esta palabra es valida");
+        } else {
+            JOptionPane.showMessageDialog(null, "Esta palabra no es valida");
+            return;
+        }
+    }
 }
